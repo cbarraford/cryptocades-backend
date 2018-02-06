8c00f1d73861b5ab43f5687cea8831a8cbd3149d
Chad Barraford <cbarraford@gmail.com>
2018-02-06 17:29:34 +0200

    fix db migration

--- migrations/1517756051_winner.up.sql
+++ migrations/1517756051_winner.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE jackpots ADD COLUMN btc_address CHAR(34) NOT NULL;
-ALTER TABLE jackpots ADD COLUMN transaction_id TEXT NOT NULL;
+ALTER TABLE jackpots ADD COLUMN btc_address CHAR(34) NOT NULL DEFAULT '';
+ALTER TABLE jackpots ADD COLUMN transaction_id TEXT NOT NULL DEFAULT '';
